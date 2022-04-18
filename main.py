1cf13d239210420caa8922c8e6615048fc5fac6f
davidvajda <davidvajda1998@gmail.com>
2022-04-18 21:09:04 +0200

    fixed queue bug

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import logging
 import os
 
 import eventlet
@@ -8,7 +7,7 @@ from q import Queue
 from person import Person
 
 # SERVER SET UP
-socket = socketio.Server(cors_allowed_origins="*", logger=True, engineio_logger=True)
+socket = socketio.Server(cors_allowed_origins="*") # , logger=True, engineio_logger=True)
 app = socketio.WSGIApp(socket, static_files={
     '/': {'content_type': 'text/html', 'filename': 'index.html'}
 })
@@ -20,7 +19,8 @@ talkees = Queue()
 listeners = Queue()
 
 # DEBUG FUNCTION, MIGHT DELETE LATER
-def check_clients():
+@socket.event
+def check_clients(sid):
     print(f"clients -> {clients}")
     print(f"reconnects -> {reconnects}")
     print(f"listeners -> ", end="")
@@ -30,8 +30,7 @@ def check_clients():
 
 @socket.event
 def connect(sid, environ):
-    logging.critical(f"[CONNECTED] {sid}")
-    # print("[CONNECTED]", sid)
+    print("[CONNECTED]", sid)
     clients[sid] = Person(sid)
     clients[sid].set_environ(environ)
     socket.emit("connected", sid)
@@ -44,8 +43,7 @@ def disconnect(sid):
     disconnected_person = clients.pop(sid)
     other_client_sid = disconnected_person.other_client_sid
 
-    logging.critical(f"[DISCONNECTED] {disconnected_person.jsonify()}")
-    # print("[DISCONNECTED]", disconnected_person.jsonify())
+    print("[DISCONNECTED]", disconnected_person.jsonify())
 
     # IF THE OTHER CLIENT IS STILL CONNECTED
     if other_client_sid in clients:
@@ -63,8 +61,6 @@ def disconnect(sid):
     elif other_client_sid in reconnects:
         reconnects.pop(other_client_sid)
 
-    check_clients()
-
 @socket.event
 def message(sid, data):
     client = clients.get(sid)
@@ -79,13 +75,13 @@ def message(sid, data):
 
 @socket.event
 def reconnect(sid, prev_sid):
-    # GET PERSON FROM RECONNECTS AND REMOVE HIM FROM THE DICT
+    # Pop person instance out of reconnects dictionary
     reconnected_client = reconnects.get(prev_sid)
     if not reconnected_client:
         return
     reconnects.pop(prev_sid)
 
-    # restore the information from old client instance
+    # restore the information from original person instance
     other_client_sid = reconnected_client.other_client_sid
     clients[sid].set_name(reconnected_client.name, reconnected_client.role)
 
@@ -93,7 +89,7 @@ def reconnect(sid, prev_sid):
     if other_client_sid not in clients:
         return
 
-    # restoring connection
+    # restoring connection 
     clients[sid].connect_to(clients[other_client_sid])
 
     # sending informatin to both clients
@@ -115,48 +111,61 @@ def talkee_join(sid, data):
     talkee = clients.get(sid)
 
     if not talkee:
+        print(f"[ERROR] Talkee {sid} not found in clients!")
         return
 
     talkee.set_name(data["name"], "talkee")
 
-    # check if can be paired with listener and add them to connection if so
-    if not listeners.is_empty():
-
+    # get available listener from queue
+    while not listeners.is_empty():
         listener = listeners.dequeue()
-        talkee.connect_to(listener)
 
-        # send information to talkee then to listener
-        socket.emit("chat_connected", listener.jsonify(), room=sid)
-        socket.emit("chat_connected", talkee.jsonify(), room=listener.sid)
+        if listener.sid in clients:
+            break
 
-    # if listener queue is empty add talkee to queue
     else:
+        # if break didn't happen - listener queue is empty, this code gets executed
         talkees.enqueue(talkee)
-        socket.emit("enqueued", room=sid)
+        socket.emit("enqueued", room=sid) 
+        return
+
+    # if break did happen
+    talkee.connect_to(listener)
+
+    # send information to both clients
+    socket.emit("chat_connected", listener.jsonify(), room=sid)
+    socket.emit("chat_connected", talkee.jsonify(), room=listener.sid)
 
 @socket.event
 def listener_join(sid, data):
     listener = clients.get(sid)
 
     if not listener:
+        print(f"[ERROR] Listener {sid} not found in clients!")
         return
 
     listener.set_name(data["name"], "listener")
 
-    # check if can be paired with talkee and add them to connection if so
-    if not talkees.is_empty():
+    # get available talkee from queue
+    while not talkees.is_empty():
 
         talkee = talkees.dequeue()
-        listener.connect_to(talkee)
 
-        # send information to listener then to talkee
-        socket.emit("chat_connected", talkee.jsonify(), room=sid)
-        socket.emit("chat_connected", listener.jsonify(), room=talkee.sid)
+        if talkee.sid in clients:
+            break
 
-    # if listener queue is empty add listener to queue
     else:
+        # if break didn't happen - talkee queue is empty, this code gets executed
         listeners.enqueue(listener)
         socket.emit("enqueued", room=sid)
+        return
+
+    # if break did happen
+    listener.connect_to(talkee)
+
+    # send information to both clients
+    socket.emit("chat_connected", talkee.jsonify(), room=sid)
+    socket.emit("chat_connected", listener.jsonify(), room=talkee.sid)
 
 @socket.event
 def leave_chat(sid):
@@ -190,5 +199,5 @@ def send_message(message = None, type = "message", time = None, to = None):
 
 if __name__ == '__main__':
     port = os.environ.get("PORT", 5000)
-    # eventlet.wsgi.server(eventlet.listen(('localhost', 5000)), app, debug=True)
-    eventlet.wsgi.server(eventlet.listen(('', int(port))), app)
+    eventlet.wsgi.server(eventlet.listen(('localhost', 5000)), app, debug=True)
+    # eventlet.wsgi.server(eventlet.listen(('', int(port))), app)
